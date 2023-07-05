72f0f9cea5d24a105662760837ab8044a0f2390b
danish tyagi <danishtyagi@yahoo.co>
2023-07-05 17:27:09 +0530

    added toggle theme function on button in theme toggle componenet using global context

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Gallery from "./Gallery";
+import ThemeToggle from "./ThemeToggle";
+import SearchForm from "./SearchForm";
+
+
+const App = () => {
+  return <main><ThemeToggle />
+  <SearchForm />
+  <Gallery />
+  </main>;
+};
+export default App;
