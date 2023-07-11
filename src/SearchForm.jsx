bc1245b0d826e35a1e6cf6a301b80aa919142780
danish tyagi <danishtyagi@yahoo.co>
2023-07-11 18:33:18 +0530

    search form functionality updated

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) {
+      return null;
+    } else {
+      setSearchTerm(searchValue);
+    }
+  };
   return (
-    <h2>SearchForm</h2>
-  )
-}
+    <section>
+      <h1 className="title">Unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="dog"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
